41f317597e1d29d52f1a6efb0d9438cc2e8015f7
Matthias <matthias.volk@rwth-aachen.de>
2014-05-16 16:27:58 +0200

    Integrated bullet engine with hello wordl example

--- src/main.cc
+++ src/main.cc
@@ -10,6 +10,10 @@
  *
  */
 
+#include <iostream>
+
+#include <btBulletDynamicsCommon.h>
+
 // OpenGL defines and function pointers. We use this instead of the system GL.h or glew.h.
 // This has to be included before glfw.h !
 #include <ACGL/OpenGL/GL.hh>
@@ -123,7 +127,7 @@ void myGLFWErrorCallback( int errorCode, const char *description )
     ACGL::Utils::error() << "GLFW error " << errorCode << ": " << description << std::endl;
 }
 
-int main( int argc, char *argv[] )
+/*int main( int argc, char *argv[] )
 {
     //
     // Initialise GLFW
@@ -216,5 +220,75 @@ int main( int argc, char *argv[] )
     glfwDestroyWindow( myWindow );
     glfwTerminate();
     exit(0);
-}
+}*/
 
+
+//Hello world example from
+//http://www.bulletphysics.org/mediawiki-1.5.8/index.php/Hello_World
+int main (void)
+{
+        btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+
+        btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+        btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+        btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+        btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher,broadphase,solver,collisionConfiguration);
+
+        dynamicsWorld->setGravity(btVector3(0,-10,0));
+
+
+        btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0,1,0),1);
+
+        btCollisionShape* fallShape = new btSphereShape(1);
+
+
+        btDefaultMotionState* groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),btVector3(0,-1,0)));
+        btRigidBody::btRigidBodyConstructionInfo
+                groundRigidBodyCI(0,groundMotionState,groundShape,btVector3(0,0,0));
+        btRigidBody* groundRigidBody = new btRigidBody(groundRigidBodyCI);
+        dynamicsWorld->addRigidBody(groundRigidBody);
+
+
+        btDefaultMotionState* fallMotionState =
+                new btDefaultMotionState(btTransform(btQuaternion(0,0,0,1),btVector3(0,50,0)));
+        btScalar mass = 1;
+        btVector3 fallInertia(0,0,0);
+        fallShape->calculateLocalInertia(mass,fallInertia);
+        btRigidBody::btRigidBodyConstructionInfo fallRigidBodyCI(mass,fallMotionState,fallShape,fallInertia);
+        btRigidBody* fallRigidBody = new btRigidBody(fallRigidBodyCI);
+        dynamicsWorld->addRigidBody(fallRigidBody);
+
+
+        for (int i=0 ; i<300 ; i++) {
+                dynamicsWorld->stepSimulation(1/60.f,10);
+
+                btTransform trans;
+                fallRigidBody->getMotionState()->getWorldTransform(trans);
+
+                std::cout << "sphere height: " << trans.getOrigin().getY() << std::endl;
+        }
+
+        dynamicsWorld->removeRigidBody(fallRigidBody);
+        delete fallRigidBody->getMotionState();
+        delete fallRigidBody;
+
+        dynamicsWorld->removeRigidBody(groundRigidBody);
+        delete groundRigidBody->getMotionState();
+        delete groundRigidBody;
+
+
+        delete fallShape;
+
+        delete groundShape;
+
+
+        delete dynamicsWorld;
+        delete solver;
+        delete collisionConfiguration;
+        delete dispatcher;
+        delete broadphase;
+
+        return 0;
+}
