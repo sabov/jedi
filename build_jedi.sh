1977bd2d528f6b67f486c08b75f2abb80edde299
MazZzinatus <matthias_volk@gmx.de>
2014-08-01 19:42:14 +0200

    Fixed collision issue

--- build_jedi.sh
+++ build_jedi.sh
@@ -7,13 +7,13 @@ cd ../..
 echo "Building GLFW successful"
 
 echo "Building Bullet"
-cd extern/bullet3
+cd extern/bullet
 /bin/bash install.sh
 cd ../..
 echo "Building Bullet successful"
 
 echo "Building Jedi"
-cmake . -DCMAKE_BUILD_TYPE=RELEASE
+cmake .
 make
 echo "Building Jedi successful"
 echo "Jedi can now be started by executing ./run_jedi.sh"
